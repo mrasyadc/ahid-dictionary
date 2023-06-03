6bdcc8fa5417243ddfd7ffac6880b88ccd77ce94
mrasyadc <mrcrasyad26@gmail.com>
2023-06-03 11:31:57 +0700

    add type mouseeventhandler

--- src/components/LanguageButton.tsx
+++ src/components/LanguageButton.tsx
@@ -1,11 +1,13 @@
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { Button, Stack, useColorMode } from "@chakra-ui/react";
+import { MouseEventHandler } from "react";
 
 export default function LanguageButton({
   isEnglish,
   onClick,
 }: {
   isEnglish: boolean;
+  onClick: MouseEventHandler;
 }): JSX.Element {
   return <Button onClick={onClick}>{isEnglish ? "EN" : "ID"}</Button>;
 }
