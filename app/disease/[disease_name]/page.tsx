99c812fdc0cb93948a4c4bc491c09d6ff539de3b
mrasyadc <mrcrasyad26@gmail.com>
2023-06-17 17:16:20 +0700

    remove metadata

--- app/disease/[disease_name]/page.tsx
+++ app/disease/[disease_name]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useEffect, useState } from "react";
 import BackButton from "@/src/components/BackButton";
 import DarkModeButton from "@/src/components/DarkModeButton";
 import DataTable from "@/src/components/DataTable";
@@ -17,24 +17,24 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+
 import useSWR from "swr";
-import { Metadata, ResolvingMetadata } from "next";
+// import { Metadata, ResolvingMetadata } from "next";
 
-type Props = {
-  params: { disease_name: string };
-};
+// type Props = {
+//   params: { disease_name: string };
+// };
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  // read route params
-  const disease_name = params.disease_name;
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//   // read route params
+//   const disease_name = params.disease_name;
 
-  console.log(disease_name);
+//   console.log(disease_name);
 
-  return {
-    title: `${disease_name} - Atlas of Human Infectious Disease`,
-  };
-}
+//   return {
+//     title: `${disease_name} - Atlas of Human Infectious Disease`,
+//   };
+// }
 
 export default function Disease({
   params,
