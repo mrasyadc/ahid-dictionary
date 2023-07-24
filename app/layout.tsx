fb2fe74fa803b551b2d31f0af5a35ddd500155ee
Muhammad Rasyad Caesarardhi <56964497+mrasyadc@users.noreply.github.com>
2023-07-24 20:53:42 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Atlas of Human Infectious Disease Dictionary",
+  "google-site-verification": "7dw65JxAOx5PFJ6qeMi2J1QWGMRoXpq52YE3XOXFJfU"
 };
 export default function RootLayout({
   children,
