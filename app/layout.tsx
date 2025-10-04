f72a0a0afccc62ba91db44960b6cd71c33902a7c
Muhammad Rasyad Caesarardhi <56964497+mrasyadc@users.noreply.github.com>
2025-10-04 23:37:36 +0700

    Add Umami analytics script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import { Providers } from "./providers";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Atlas of Human Infectious Disease Dictionary"
@@ -15,6 +16,11 @@ export default function RootLayout({
       <body>
         <Providers>{children}</Providers>
       </body>
+      <Script
+        defer
+        src='https://umami.mrasyadc.com/script.js'
+        data-website-id='69eb7755-e2fc-4f82-989d-77dc19f3d443'
+      />
     </html>
   );
 }
